7c97b8be6fdf63f16f41425d11cd445f49d20892
Benjamin Post <benjaminpost@ymail.com>
2021-06-08 19:09:13 +0200

    small updates to setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
 
 requirements = [
-    # package requirements go here
+    "attrs >= 18.1.0",
+    "dask",
+    "numpy",
+    "xarray >= 0.10.0",
+    "zarr >= 2.3.0",
+    "xarray-simlab",
+    "scipy",
 ]
 
 setup(
     name='xarray-simlab-ode',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="Xarray-simlab extension for building and running models based on ordinary differential equations",
+    description="Framework for building and solving ODE-based models, an extension of xarray-simlab",
     license="BSD",
     author="Benjamin Post",
     author_email='ben@anoutpost.com',
     url='https://github.com/ben1post/xarray-simlab-ode',
-    packages=['xso'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'xso=xso.cli:cli'
         ]
     },
     install_requires=requirements,
-    keywords='xarray-simlab-ode',
+    tests_require=["pytest >= 3.3.0"],
+    keywords='xarray-simlab python xarray modelling simulation framework ode',
     classifiers=[
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ]
